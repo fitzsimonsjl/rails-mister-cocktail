41338b61b0e37cd9c70a81025443650b6a5d46d4
Jack Fitzsimons <fitzsimonsjal@gmail.com>
2019-11-07 20:26:44 +0100

    all working except reviews

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,27 @@
+class DosesController < ApplicationController
+   def new
+    # we need @ingredient in our `simple_form_for`
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `ingredient_id` to associate dose with corresponding ingredient
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "cocktails/show"
+    end
+  end
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
